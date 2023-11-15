c73d132fa45e19e29e32da818a68dcd2eef42cbd
Banyc <36535895+Banyc@users.noreply.github.com>
2023-11-15 23:48:51 +0800

    feat: const constructors

--- src/canvas/real.rs
+++ src/canvas/real.rs
@@ -7,7 +7,7 @@ pub struct RealSpace {
 }
 
 impl RealSpace {
-    pub fn new(x_axis_range: RangeInclusive<f64>, y_axis_range: RangeInclusive<f64>) -> Self {
+    pub const fn new(x_axis_range: RangeInclusive<f64>, y_axis_range: RangeInclusive<f64>) -> Self {
         Self {
             x_axis_range,
             y_axis_range,
@@ -30,7 +30,7 @@ pub struct RealPoint {
 }
 
 impl RealPoint {
-    pub fn new(x: f64, y: f64) -> Self {
+    pub const fn new(x: f64, y: f64) -> Self {
         Self { x, y }
     }
 
